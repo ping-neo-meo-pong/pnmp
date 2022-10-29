46c3fa13b961a117cdc96395359a1aed77504963
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-10-29 13:29:26 +0900

    FEATURE: docker에서 db, back, front 연동

---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: '4242',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
