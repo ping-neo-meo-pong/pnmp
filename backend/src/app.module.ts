54e89e1f308f151fe8ab7eb4cf7c3532bdf90931
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-11-01 00:49:49 +0900

    REFACTOR: 환경변수 추출

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,17 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: '4242',
-      database: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_CONTAINER_PORT, 10) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       entities: [User],
       synchronize: true,
       autoLoadEntities: true,
