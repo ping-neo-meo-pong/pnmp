b4d7a040dd999de879a98a12fbd6bf57d33d579f
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-11-12 12:08:45 +0900

    REFACT: npm install 의존성 제거

--- srcs/backend.sh
+++ srcs/backend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ ! -d "node_modules" ];then
-  npm install
-fi
+# if [ ! -d "node_modules" ];then
+#   npm install
+# fi
 
+npm install
 npm install @nestjs/cli 
 exec npm run start:dev
--- srcs/frontend.sh
+++ srcs/frontend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ ! -d "node_modules" ];then
-  npm install
-fi
+# if [ ! -d "node_modules" ];then
+#   npm install
+# fi
 
+npm install
 exec npm run dev
