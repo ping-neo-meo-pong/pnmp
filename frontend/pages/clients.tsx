89c60e8021e1333e12cad5af891d247c8425f077
minsikim-42 <spalstlr321@naver.com>
2022-11-14 15:02:55 +0900

    button for go to dm

--- frontend/pages/clients.tsx
+++ frontend/pages/clients.tsx
@@ -2,6 +2,9 @@
 import Router, { useRouter } from "next/router";
 import { useEffect } from "react";
 import { io, Socket } from "socket.io-client";
+import axios from "axios";
+
+import { user_data } from "./login";
 
 export let socket: Socket;
 export default function Client() {
@@ -16,12 +19,48 @@ export default function Client() {
     event.preventDefault();
     let message = event.currentTarget.message.value;
     socket.emit("hello", message);
-    router.push("/dm");
   }
 
   return (
     <div>
       <h1>Socket.io</h1>
+      <GoToDmRoom />
+    </div>
+  );
+}
+
+function GoToDmRoom() {
+  let router = useRouter();
+  let result: JSX.Element[] = [];
+  function onClickDmRoom() {
+    router.push("/dm");
+  }
+  useEffect(() => {
+    let rooms;
+    axios
+      .get("/api/dm", {
+        headers: {
+          Authorization: `Bearer ${user_data._token}`,
+        },
+      })
+      .then(function (response) {
+        rooms = response.data;
+        console.log("room:");
+        console.log(rooms);
+        for (let i = 0; rooms[i]; i++) {
+          console.log(`room! for${i}`);
+          result.push(<div>Room</div>);
+        }
+      })
+      .catch(() => {
+        router.push("/login");
+      });
+  }, []);
+  console.log("retult:");
+  console.log(result);
+  return (
+    <div>
+      <button onClick={onClickDmRoom}>go to dm room</button>
     </div>
   );
 }
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,55 +1,60 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 // import styles from '../styles/Home.module.css'
-import React, { FormEvent, SyntheticEvent } from 'react';
-import { useRouter } from 'next/dist/client/router';
-import { useState } from 'react';
+import React, { FormEvent, SyntheticEvent } from "react";
+import { useRouter } from "next/dist/client/router";
+import { useState } from "react";
 import Link from "next/link";
-import axios from 'axios';
-import { Socket } from 'socket.io-client';
+import axios from "axios";
+import { Socket } from "socket.io-client";
 
-let user_data = {
-	_name : "",
-	_pass : "",
-	_socket : "",
-	_token : ""
-}
+export let user_data = {
+  _name: "",
+  _pass: "",
+  _socket: "",
+  _token: "",
+};
 
 export default function Login() {
-	const router = useRouter();
+  const router = useRouter();
 
-	function onSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		console.log("hi");
-		console.log(event.currentTarget.username.value);
-		// debugger;
-		// console.log(event.target);
-		axios.post('/api/auth/login', {
-			username: event.currentTarget.username.value,
-			password: event.currentTarget.password.value
-		}).then(function (response) {
-			router.push("/clients");
-			user_data._token = response.data.access_token;
-			user_data._name = event.currentTarget.username.value;
-			user_data._pass = event.currentTarget.password.value;
-			console.log(user_data);
-		}).catch(function (error) {
-			// alert(error);
-			console.log(error);
-		});
-
-	}
-	return (
-		<div>
-			<form id="username" action="/api/auth/login" method="post"
-				onSubmit={onSubmitHandler}>
-				<input type="text" id="username" name="username" /><br />
-				<input type="text" id="password" name="password" />
-				<button type="submit">
-					Login
-				</button>
-			</form>
-			{/* <button onClick={onClickHandler}>로그인</button> */}
-		</div>
-	);
+  function onSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("hi");
+    console.log(event.currentTarget.username.value);
+    // debugger;
+    // console.log(event.target);
+    axios
+      .post("/api/auth/login", {
+        username: event.currentTarget.username.value,
+        password: event.currentTarget.password.value,
+      })
+      .then(function (response) {
+        router.push("/clients");
+        user_data._token = response.data.access_token;
+        user_data._name = event.currentTarget.username.value;
+        user_data._pass = event.currentTarget.password.value;
+        console.log(user_data);
+      })
+      .catch(function (error) {
+        // alert(error);
+        console.log(error);
+      });
+  }
+  return (
+    <div>
+      <form
+        id="username"
+        action="/api/auth/login"
+        method="post"
+        onSubmit={onSubmitHandler}
+      >
+        <input type="text" id="username" name="username" />
+        <br />
+        <input type="text" id="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+      {/* <button onClick={onClickHandler}>로그인</button> */}
+    </div>
+  );
 }
