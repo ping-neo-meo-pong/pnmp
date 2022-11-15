9afa7d2eddb4d1ed4cef7e7dc275d8bc1575b544
jwoo <74581396+Jiwon-Woo@users.noreply.github.com>
2022-11-15 18:50:52 +0900

    Delete Channel.repository.ts

--- backend/src/core/channel/Channel.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { Channel } from './channel.entity';
-import { CustomRepository } from '../../typeorm-ex.decorator';
-
-@CustomRepository(Channel)
-export class ChannelRepository extends Repository<Channel> {}
