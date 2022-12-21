c62368292eaf9ed4cd66deef325133f668ee2bed
kimhxsong <kimhxsong@gmail.com>
2022-12-21 17:42:20 +0900

    feat: add project logo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import Image from "next/image";
 // import styles from '../styles/Home.module.css'
-import React from "react";
 import { useRouter } from "next/dist/client/router";
 import { useState, useEffect } from "react";
 // import Link from "next/link";
 import { isLoggedIn, logout } from "../lib/login";
 
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -16,10 +13,9 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
+import Typography from '@mui/joy/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 
 // export default function Home() {
 //   const router = useRouter();
@@ -32,6 +28,24 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 // }
 
 import { useSession, signIn, signOut } from "next-auth/react"
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Button from '@mui/joy/Button';
+import Sheet from '@mui/joy/Sheet';
+import Image from 'next/image'
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://localhost">
+        ping-neo-meo-pong
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 export default function Component() {
   const { data: session } = useSession()
@@ -45,31 +59,32 @@ export default function Component() {
   }
   return (
     <>
-         <Box
+       <CssVarsProvider>
+        <Sheet variant="outlined"
           sx={{
-            marginTop: 8,
+            width: 300,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & botom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-                  <Button
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => signIn('github')}
-            >
-              Sign With GitHub
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+          }}>
+            <div  style={{display: "flex", justifyContent: "center"}}>
+            <Image src="/pu.svg" alt="home" width={220} height={180}/>
+            </div>
+          <Button onClick={() => {signIn('github')}}>
+            Sign with GitHub
             </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => signIn('42-school')}
-            >
-              Sign With 42School
+          <Button onClick={() => {signIn('42-school')}}>
+            Sign with 42school
             </Button>
-      </Box>
+        </Sheet>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </CssVarsProvider>
     </>
   )
 }
