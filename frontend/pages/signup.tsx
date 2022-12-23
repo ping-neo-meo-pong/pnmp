ce8fb5440b295f5e2f207baab30892010161960a
minsikim-42 <spalstlr321@naver.com>
2022-12-23 21:16:21 +0900

    feat: implement event handler (#36)

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,25 +1,28 @@
-import * as React from 'react';
-import { useState } from "react"
-import Image from 'next/image';
-import Typography from '@mui/joy/Typography';
-import TextField from '@mui/joy/TextField';
-import Sheet from '@mui/joy/Sheet';
-import Link from '@mui/joy/Link';
-import FormControl from '@mui/joy/FormControl';
-import Button from '@mui/joy/Button';
-import Avatar from '@mui/joy/Avatar';
-;
-import { CssVarsProvider } from '@mui/joy/styles';
+import * as React from "react";
+import { useState } from "react";
+import Image from "next/image";
+import Typography from "@mui/joy/Typography";
+import TextField from "@mui/joy/TextField";
+import Sheet from "@mui/joy/Sheet";
+import Link from "@mui/joy/Link";
+import FormControl from "@mui/joy/FormControl";
+import Button from "@mui/joy/Button";
+import Avatar from "@mui/joy/Avatar";
+import { CssVarsProvider } from "@mui/joy/styles";
+import axios from "axios";
 
 function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link href="https://localhost">
-        ping-neo-meo-pong
-      </Link>{' '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link href="https://localhost">ping-neo-meo-pong</Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -27,6 +30,7 @@ function Copyright(props: any) {
 export default function SignUp() {
   const [createObjectURL, setCreateObjectURL] = useState(null);
   const [image, setImage] = useState(null);
+  const [userName, setUserName] = useState("");
 
   const uploadToClient = (event) => {
     if (event.target.files && event.target.files[0]) {
@@ -38,53 +42,78 @@ export default function SignUp() {
     console.log("I'm Here");
   };
 
-  const handleSubmit = async (event) => {
-      const body = new FormData();
-      body.append("file", image);
-      const response = await fetch("/api/file", {
+  const handleSubmit = async () => {
+    const body = new FormData();
+    body.append("file", image);
+    axios
+      .post(`/server/api/file?${userName}`, {
         method: "POST",
-        body
-      });
+        body,
+      })
+      .then((res) => {})
+      .catch((e) => {});
   };
 
   return (
-      <CssVarsProvider>
+    <CssVarsProvider>
+      {/* <FormControl onSubmit={handleSubmit}> */}
       <FormControl>
-      <Sheet variant="outlined"
+        <Sheet
+          variant="outlined"
           sx={{
             width: 300,
-            mx: 'auto', // margin left & right
+            mx: "auto", // margin left & right
             my: 4, // margin top & botom
             py: 3, // padding top & bottom
             px: 2, // padding left & right
-            display: 'flex',
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
             gap: 2,
-            borderRadius: 'sm',
-            boxShadow: 'md',
-            alignItems: 'center',
-          }}>
-          <Image src="/pu.svg" alt="home" width={220} height={180}/>
-          <Avatar src= {createObjectURL} size="lg" sx={{
-            width: 180,
-            height: 180,
-            mt: 'lg',
-            mx: 'auto', // margin left & right
-            display: 'flex',
-            flexDirection: 'column',
-            boxShadow: 'sm',
-            alignItems: 'center',
-          }}/>
-        <Button variant="plain" component="label">
-          Change Profile Image
-          <input hidden type="file" name="myImage" onChange={uploadToClient} />
-        </Button>
-        <TextField placeholder='Username' sx={{width: 300}} />
-        <Button onClick={() => {handleSubmit}} sx={{width: 300}}>
-          Sign Up
-        </Button>
-      </Sheet>
-        </FormControl>
+            borderRadius: "sm",
+            boxShadow: "md",
+            alignItems: "center",
+          }}
+        >
+          <Image src="/pu.svg" alt="home" width={220} height={180} />
+          <Avatar
+            src={createObjectURL}
+            size="lg"
+            sx={{
+              width: 180,
+              height: 180,
+              mt: "lg",
+              mx: "auto", // margin left & right
+              display: "flex",
+              flexDirection: "column",
+              boxShadow: "sm",
+              alignItems: "center",
+            }}
+          />
+          <Button variant="plain" component="label">
+            Change Profile Image
+            <input
+              hidden
+              type="file"
+              name="myImage"
+              onChange={uploadToClient}
+            />
+          </Button>
+          <TextField
+            value={userName}
+            placeholder="Username"
+            sx={{ width: 300 }}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <Button
+            onClick={() => {
+              handleSubmit();
+            }}
+            sx={{ width: 300 }}
+          >
+            Sign Up
+          </Button>
+        </Sheet>
+      </FormControl>
       <Copyright sx={{ mt: 2, mb: 4 }} />
     </CssVarsProvider>
   );
