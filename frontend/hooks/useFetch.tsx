c7f2dc303365ccf9aa4c2b0f10d5bc20f8ea7564
kimhxsong <kimhxsong@gmail.com>
2022-12-25 20:34:25 +0900

    feat: add useFetch (#47)

---
+++ frontend/hooks/useFetch.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+export default function useFetch(url: string) {
+  const [responseJSON, setResponseJSON] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect((): any => {
+    let shouldCancel = false;
+
+    const callFetch = async () => {
+      setIsLoading(true);
+      try {
+        const response: AxiosResponse = await axios.get(url);
+        const newResponseJSON = response.data;
+        if (shouldCancel) return;
+        setResponseJSON(newResponseJSON);
+        setError(null);
+      } catch (error: any) {
+        if (shouldCancel) return;
+        setError(error);
+        setResponseJSON(null);
+      }
+
+      setIsLoading(false);
+    };
+
+    callFetch();
+
+    return () => (shouldCancel = true);
+  }, [url]);
+
+  return {
+    responseJSON,
+    isLoading,
+    error,
+  };
+}
