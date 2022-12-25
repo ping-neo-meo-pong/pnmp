3ca091d6c05641c23029dc204687a89cffecdd02
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-12-25 18:07:35 +0900

    feat: upload profile image to server and sign up user (#54)

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -6,6 +6,9 @@ import {
   Request,
   Res,
   Req,
+  UseInterceptors,
+  Query,
+  UploadedFile,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -13,6 +16,44 @@ import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { LoginReqDto } from 'src/api/user/dto/login-req.dto';
 import { UserRepository } from 'src/core/user/user.repository';
 import { UserStatus } from 'src/enum/user-status';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { existsSync, mkdirSync } from 'fs';
+import { BadRequestException } from '@nestjs/common';
+
+const multerOptions = {
+  fileFilter: (request, file, callback) => {
+    if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+      callback(null, true);
+    } else {
+      callback(
+        new BadRequestException('이미지 형식은 jpg, jpeg, png만 허용'),
+        false,
+      );
+    }
+  },
+
+  storage: diskStorage({
+    filename: (request, file, callback) => {
+      const filename = file.originalname.split('.');
+      const editName = `${Date.now()}_${request.query.email}.${
+        filename[filename.length - 1]
+      }`;
+      callback(null, editName);
+    },
+    destination: (request, file, callback) => {
+      const uploadPath = 'upload';
+      if (!existsSync(uploadPath)) {
+        mkdirSync(uploadPath);
+      }
+      callback(null, uploadPath);
+    },
+  }),
+
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 10 Mb
+  },
+};
 
 @Controller('auth')
 @ApiTags('auth')
@@ -22,12 +63,36 @@ export class AuthController {
     private userRepository: UserRepository,
   ) {}
 
+  @Post('/signup')
+  @UseInterceptors(FileInterceptor('profileImage', multerOptions))
+  async signup(
+    @Query() userInfo,
+    @UploadedFile() file: Express.Multer.File,
+    @Res({ passthrough: true }) res,
+  ) {
+    console.log(file);
+    const user = this.authService.signUpUser(
+      userInfo.username,
+      userInfo.email,
+      file?.filename ?? null,
+    );
+    const token = await this.authService.getToken(user);
+    res.cookie('jwt', token.accessToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+    return { ...user, ...token };
+  }
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   @ApiConsumes('application/json')
   @ApiBody({ type: LoginReqDto })
   async login(@Req() req, @Res({ passthrough: true }) res) {
     const user = req.user;
+    if (user.firstLogin) {
+      return user;
+    }
     const token = await this.authService.getToken(user);
     // res.setHeader('Authorization', 'Bearer ' + token.accessToken);
     res.cookie('jwt', token.accessToken, {
